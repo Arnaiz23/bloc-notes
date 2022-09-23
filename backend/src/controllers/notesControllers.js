e740a00acb711c854ef043eaff4a3eb622067636
Arnaiz23 <aaf110501@gmail.com>
2022-09-23 18:48:11 +0200

    feat: v1.0.1   * Catch the exceptions/errors in the getNotes and createNote     controllers

--- backend/src/controllers/notesControllers.js
+++ backend/src/controllers/notesControllers.js
@@ -5,6 +5,12 @@ const {
 
 async function getNotes(req, res) {
   const notes = await getNotesService()
+  if (notes.length < 0) {
+    return res.status(505).send({
+      status: "ERROR",
+      data: "Error with the notes store",
+    })
+  }
   res.status(200).send({
     status: "OK",
     data: notes,
@@ -13,6 +19,28 @@ async function getNotes(req, res) {
 
 const createNote = async (req, res) => {
   const body = req.body
+
+  if (
+    typeof body.title === "undefined" ||
+    body.title.length === 0 ||
+    typeof body.title !== "string"
+  ) {
+    return res.status(404).send({
+      status: "ERROR",
+      data: "Error in the TITLE field",
+    })
+  }
+
+  if (
+    typeof body.content === "undefined" ||
+    body.content.length === 0 ||
+    typeof body.content !== "string"
+  ) {
+    return res.status(404).send({
+      status: "ERROR",
+      data: "Error in the CONTENT field",
+    })
+  }
 
   const newNote = await createNoteService(body)
 
