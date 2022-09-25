0a61a2e1892b9c86a2e2c502a351ae7802066a40
Arnaiz23 <aaf110501@gmail.com>
2022-09-25 13:23:54 +0200

    test: 💍 v1.0.20
    Modify the test with a new structure

--- backend/src/tests/notes.test.js
+++ backend/src/tests/notes.test.js
@@ -2,13 +2,13 @@ const supertest = require("supertest")
 const mongoose = require("mongoose")
 
 const { app, server } = require("../index")
+const Note = require("../database/schemas/Notes")
 
 const api = supertest(app)
 
 const url = "/api/v1/notes"
-const idNote = "/632f84e671fdbdcc998baa7a"
 const newNote = {
-  title: "Prueba Test",
+  title: "Create Note Test",
   content: "Esta es una nueva nota y esta creada por el test",
 }
 const newNoteError = {
@@ -16,55 +16,72 @@ const newNoteError = {
   content: "Esta es una nueva nota y esta creada por el test",
 }
 
-describe("/notes getAllNotes", () => {
-  it("getAllNotes (500)", async () => {
-    await api.get(url).expect(500)
-  })
+/* Create an array for not depend of the state of the database at the moment that execute the tests */
+const initialNotes = [
+  {
+    title: "NoteTest",
+    content: "Esta es una nueva nota y esta muy bien",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    folder_id: "",
+  },
+  {
+    title: "NoteTest2",
+    content: "Esta es una nueva nota y esta muy bien",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    folder_id: "",
+  },
+]
 
-  it("getAllNotes (200)", async () => {
+beforeEach(async () => {
+  /* Delete all the notes */
+  await Note.deleteMany({})
+
+  const note1 = new Note(initialNotes[0])
+  await note1.save()
+
+  const note2 = new Note(initialNotes[1])
+  await note2.save()
+})
+
+describe("/notes getAllNotes", () => {
+  it("getAllNotes", async () => {
     await api
       .get(url)
       .expect(200)
       .expect("Content-type", /application\/json/)
   })
+
+  it("getAllNotes have as minimum 2 notes", async () => {
+    const response = await api.get(url)
+    expect(response.body.data).toHaveLength(2)
+  })
 })
 
 describe("/notes getOneNote", () => {
-  /* ONE NOTE (Markdown 2 -> 632f84e671fdbdcc998baa7a) */
-
-  it("getOneNote (500)", async () => {
+  it("getOneNote (404) badID", async () => {
+    const badId = "/asas"
     await api
-      .get(url + idNote)
-      .expect(500)
-      .expect("Content-type", /application\/json/)
-  })
-  it("getOneNote (404)", async () => {
-    await api
-      .get(url + idNote)
+      .get(url + badId)
       .expect(404)
       .expect("Content-type", /application\/json/)
   })
   it("getOneNote (200)", async () => {
+    const response = await api.get(url)
+    const { data } = response.body
     await api
-      .get(url + idNote)
+      .get(url + "/" + data[0]._id)
       .expect(200)
       .expect("Content-type", /application\/json/)
   })
 })
 
 describe("/notes create Note", () => {
-  it("Create note (500)", async () => {
+  it("Create note (400) title empty", async () => {
     await api
       .post(url)
-      .send(newNote)
-      .expect(500)
-      .expect("Content-type", /application\/json/)
-  })
-
-  it("Create note (400)", async () => {
-    await api
-      .post(url)
-      .send(newNote)
+      .send(newNoteError)
       .expect(400)
       .expect("Content-type", /application\/json/)
   })
