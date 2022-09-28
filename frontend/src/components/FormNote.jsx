9023c2b28eaec568fd475114b755337352c0e8e8
Arnaiz23 <aaf110501@gmail.com>
2022-09-28 13:58:47 +0200

    feat: 🎸 v1.0.52
    Create the FormNote for the third column

---
+++ frontend/src/components/FormNote.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import {useLocation} from "wouter";
+
+import {createNote} from "../services/Notes";
+
+export default function FormNote({update, setUpdate}) {
+    const [newNote, setNewNote] = useState({
+        "title": "",
+        "content": ""
+    })
+    const setLocation = useLocation()[1]
+
+    const handleChange = (e) => {
+        setNewNote({
+            ...newNote,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        if (newNote.title.length <= 0) {
+            return alert("The title cannot be empty")
+        }
+
+        if (newNote.content.length <= 0) {
+            return alert("The content cannot be empty")
+        }
+        const newNoteCreated = await createNote(newNote)
+        update ? setUpdate(false) : setUpdate(true)
+        alert("Note created successfully")
+        setLocation(`/all/${newNoteCreated.data._id}`)
+    }
+
+    return (
+        <form className="w-full flex flex-col justify-start items-start relative" onSubmit={handleSubmit}>
+            <input type="text" placeholder="Note Name" onChange={handleChange} name="title" className="w-full p-3 pl-5 focus:outline-none text-xl bg-transparent" />
+            {/* Here maybe I delete the line and add the folder or anything */}
+            <div className="border-b border-gray-400 my-1 w-[90%] mx-auto"></div>
+            <textarea placeholder="Write the content of the note" onChange={handleChange} name="content" className="w-full h-full resize-none p-3 pl-5 text-lg bg-transparent focus:outline-none"></textarea>
+            <button className="p-2 rounded-md bg-green-600 absolute bottom-3 right-3">Save</button>
+        </form>
+    )
+}
--- frontend/src/pages/NewNote.jsx
+++ frontend/src/pages/NewNote.jsx
@@ -1,18 +1,13 @@
 import React, {useEffect, useState} from "react";
-import {useLocation} from "wouter";
+import FormNote from "../components/FormNote";
 
 import NotesColumn from "../components/NotesColumn";
 import NotesPreview from "../components/NotesPreview";
-import {getAllNotes, createNote} from "../services/Notes";
+import {getAllNotes} from "../services/Notes";
 
 export default function NewNotePage() {
   const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState({
-    "title": "",
-    "content": ""
-  })
   const [update, setUpdate] = useState(false)
-  const setLocation = useLocation()[1]
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,29 +16,6 @@ export default function NewNotePage() {
     }
     fetchData()
   }, [update])
-
-  const handleChange = (e) => {
-    setNewNote({
-      ...newNote,
-      [e.target.name]: e.target.value
-    })
-  }
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    if (newNote.title.length <= 0) {
-      return alert("The title cannot be empty")
-    }
-
-    if (newNote.content.length <= 0) {
-      return alert("The content cannot be empty")
-    }
-    const newNoteCreated = await createNote(newNote)
-    update ? setUpdate(false) : setUpdate(true)
-    alert("Note created successfully")
-    setLocation(`/all/${newNoteCreated.data._id}`)
-  }
 
   return (
     <>
@@ -56,13 +28,7 @@ export default function NewNotePage() {
           </div>
         }
       </NotesColumn>
-      <form className="w-full flex flex-col justify-start items-start relative" onSubmit={handleSubmit}>
-        <input type="text" placeholder="Note Name" onChange={handleChange} name="title" className="w-full p-3 pl-5 focus:outline-none text-xl bg-transparent" />
-        {/* Here maybe I delete the line and add the folder or anything */}
-        <div className="border-b border-gray-400 my-1 w-[90%] mx-auto"></div>
-        <textarea placeholder="Write the content of the note" onChange={handleChange} name="content" className="w-full h-full resize-none p-3 pl-5 text-lg bg-transparent focus:outline-none"></textarea>
-        <button className="p-2 rounded-md bg-green-600 absolute bottom-3 right-3">Save</button>
-      </form>
+      <FormNote update={update} setUpdate={setUpdate} />
     </>
   )
 }
