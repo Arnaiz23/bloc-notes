166f2579dd47f7cfcda4d46b5140175de361d63f
Arnaiz23 <aaf110501@gmail.com>
2022-09-28 12:54:39 +0200

    feat: 🎸 v1.0.50
    If the folders length is 0, hide the plus option

--- frontend/src/components/ListMenu.jsx
+++ frontend/src/components/ListMenu.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 import {FaFolder, FaMinusCircle, FaPlusCircle} from "react-icons/fa";
 
 import TextMenu from "./TextMenu";
-import { getFolders } from "../services/Folders";
+import {getFolders} from "../services/Folders";
 
 export default function ListMenu({icon, title}) {
   const [show, setShow] = useState(false)
@@ -14,7 +14,7 @@ export default function ListMenu({icon, title}) {
 
   useEffect(() => {
     getFolders().then(({data}) => setFolders(data))
-  },[])
+  }, [])
 
   return (
     <div className="flex align-center justify-start gap-4 p-2 cursor-pointer flex-col">
@@ -23,16 +23,20 @@ export default function ListMenu({icon, title}) {
           <i>{icon}</i>
           <h4>{title}</h4>
         </div>
-        <i>{show ? <FaMinusCircle /> : <FaPlusCircle />}</i>
+        {folders.length > 0 ?
+          <i>{show ? <FaMinusCircle /> : <FaPlusCircle />}</i>
+          :
+          <p className="opacity-70">0</p>
+        }
       </div>
       <ul className={`flex-col gap-2 p-1 ${show ? 'flex' : 'hidden'}`}>
         {folders.length > 0 &&
-            folders.map(folder => 
+          folders.map(folder =>
             <li key={folder._id} className="flex items-center justify-between">
               <TextMenu title={folder.name} icon={<FaFolder />} link={`/${folder._id}`} />
               <span className="opacity-[0.6]">{folder.length}</span>
             </li>
-            )
+          )
         }
       </ul>
     </div>
