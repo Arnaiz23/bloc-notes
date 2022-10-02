e74199dfc9f5130ee0a2282416c0c4d1d297bc10
Arnaiz23 <aaf110501@gmail.com>
2022-10-02 14:34:08 +0200

    fix: 🐛 Date format in Notes preview

--- frontend/src/components/NotesPreview.jsx
+++ frontend/src/components/NotesPreview.jsx
@@ -6,7 +6,7 @@ export default function NotesPreview({note, active, url}) {
     <Link href={`/${url}/${note._id}`}>
       <div className={`w-full ${active ? 'bg-orange-100 dark:bg-zinc-600' : 'bg-orange-200 dark:bg-zinc-700'} flex flex-col items-start justify-evenly gap-1 p-2 border-b border-white cursor-pointer`}>
         <h3 className="text-lg">{note.title}</h3>
-        <time className="text-orange-500 dark:text-orange-300 text-sm">{note.updatedAt}</time>
+        <time className="text-orange-500 dark:text-orange-300 text-sm">{new Date(String(note.updatedAt)).toLocaleString()}</time>
         <p className="text-slate-400 w-full overflow-hidden whitespace-nowrap text-ellipsis">{note.content}</p>
       </div>
     </Link>
