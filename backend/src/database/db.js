ac2c033835cb6d7ef9aaa708812621f5f7241789
Arnaiz23 <aaf110501@gmail.com>
2022-10-04 14:03:59 +0200

    fix: 🐛 Update the db connection in the backend
    Implement a NODE_ENV with differents DB URLS for dev or prod

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose")
 
+let url
+if (process.env.NODE_ENV === "prod") {
+  url = `mongodb+srv://${process.env.USER_BBDD}:${process.env.PASSWORD_BBDD}@${process.env.URL_BBDD}/${process.env.DB_BBDD}?retryWrites=true&w=majority`
+} else {
+  url = `mongodb://${process.env.URL_BBDD}:${process.env.PORT_BBDD}/${process.env.DB_BBDD}`
+}
+
 mongoose
-  .connect(
-    `mongodb://${process.env.URL_BBDD}:${process.env.PORT_BBDD}/${process.env.DB_BBDD}`,
-    { useNewUrlParser: true }
-  )
+  .connect(url, { useNewUrlParser: true })
   .then((db) => console.log("📔 Database connected"))
   .catch((err) => console.log(err))
